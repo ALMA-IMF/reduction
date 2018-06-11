7effa637a557fdd99a4ac635ff688f786f148c89
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-06-11 09:16:58 -0600

    update retrieve_weblog script to account for changes to the archive

--- reduction/retrieve_weblogs.py
+++ reduction/retrieve_weblogs.py
@@ -16,9 +16,6 @@ alma.login(username)
 
 results = alma.query(payload=dict(project_code='2017.1.01355.L'), public=False, cache=False)
 
-band3 = results['Band'] == 3
-band6 = results['Band'] == 6
-
 existing_tarballs = glob.glob("2017.1.01355.L/weblog_tarballs/*weblog.tgz")
 mouses = results['Member ous id']
 mouses_filtered = [x for x in mouses
@@ -27,33 +24,21 @@ mouses_filtered = [x for x in mouses
 
 print("Found {0} new files out of {1}".format(len(mouses_filtered), len(mouses)))
 
-# band3files = alma.stage_data(results['Member ous id'][band3])
-# band6files = alma.stage_data(results['Member ous id'][band6])
 files = alma.stage_data(mouses_filtered)
 
-#band3product_mask = np.array(['asdm' not in row['URL'] for row in band3files])
-#band6product_mask = np.array(['asdm' not in row['URL'] for row in band6files])
 product_mask = np.array(['asdm' not in row['URL'] for row in files])
 
-#band3product = band3files[band3product_mask]
-#band6product = band6files[band6product_mask]
 product = files[product_mask]
-print(product)
+print("Found {0} total product files to download".format(product))
+weblog_mask = np.array(['weblog.tgz' in row['URL'] for row in product], dtype='bool')
+weblog_files = product[weblog_mask]
+print("Found {0} weblogs to download".format(len(weblog_files)))
+weblog_fns = [x.split("/")[-1] for x in weblog_files['URL']]
+existing_weblog_fns = [x.split("/")[-1] for x in existing_tarballs]
+weblog_urls_to_download = [row['URL'] for row,fn in zip(weblog_files, weblog_fns) if fn not in existing_weblog_fns]
+print("Found {0} *new* weblogs to download".format(len(weblog_urls_to_download)))
 
-#band3tarballs = alma.download_files(band3product['URL'])
-#band6tarballs = alma.download_files(band6product['URL'])
-tarballs = alma.download_files(product['URL'])
-
-#weblogs_band3 = alma.get_files_from_tarballs(band3tarballs,
-#                                             path='.',
-#                                             regex=r'.*\.weblog.tgz')
-#
-#weblogs_band6 = alma.get_files_from_tarballs(band6tarballs,
-#                                             path='.',
-#                                             regex=r'.*\.weblog.tgz')
-weblogs = alma.get_files_from_tarballs(tarballs,
-                                       path='.',
-                                       regex=r'.*\.weblog.tgz')
+weblog_tarballs = alma.download_files(weblog_urls_to_download)
 
 if not os.path.exists('2017.1.01355.L'):
     os.mkdir('2017.1.01355.L')
@@ -61,7 +46,7 @@ if not os.path.exists('2017.1.01355.L/weblog_tarballs'):
     os.mkdir('2017.1.01355.L/weblog_tarballs')
 
 #weblogs = weblogs_band3+weblogs_band6
-for logfile in weblogs:
+for logfile in weblog_tarballs:
     print(logfile)
     with tarfile.open(logfile) as tf:
         tf.extractall('2017.1.01355.L')
