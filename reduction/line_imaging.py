2dc5a83138e564b031a7ed2d3ed50220b2dcc255
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-11-05 18:05:23 -0700

    line imaging script: center & pick correct imsize

--- reduction/line_imaging.py
+++ reduction/line_imaging.py
@@ -19,6 +19,7 @@ You can set the following environmental variables for this script:
 
 import json
 import os
+import numpy as np
 from tasks import tclean, uvcontsub
 from parse_contdotdat import parse_contdotdat, freq_selection_overlap
 
@@ -51,6 +52,81 @@ def is_7m(ms):
     else:
         return False
 
+def get_indiv_phasecenter(ms, field):
+    """
+    Get the phase center of an individual field in radians
+    """
+    msmd.open(ms)
+    field_matches = np.array([fld == field for fld in msmd.fieldnames()], dtype=bool)
+    field_ids, = np.where(field_matches)
+    ptgctrs = [msmd.phasecenter(ii) for ii in field_ids]
+    mean_ra = np.mean([pc['m0']['value'] for pc in ptgctrs])
+    mean_dec = np.mean([pc['m1']['value'] for pc in ptgctrs])
+    csys = ptgctrs[0]['refer']
+    msmd.close()
+
+    return mean_ra, mean_dec, csys
+
+def determine_phasecenter(ms, field):
+    """
+    Identify the correct phasecenter for the MS (apparently, if you don't do
+    this, the phase center is set to some random pointing in the mosaic)
+    """
+
+    if isinstance(ms, list):
+        results = [get_indiv_phasecenter(vis, field) for vis in ms]
+        csys = results[0][2]
+
+        mean_ra = np.mean([ra for ra, dec, csys in results])
+        mean_dec = np.mean([dec for ra, dec, csys in results])
+    else:
+        mean_ra, mean_dec, csys = get_indiv_phasecenter(ms, field)
+
+    return (csys, mean_ra*180/np.pi, mean_dec*180/np.pi)
+
+def get_indiv_imsize(ms, field, phasecenter, spw=0, pixscale=0.05):
+
+    cen_ra, cen_dec = phasecenter
+
+    msmd.open(ms)
+
+    field_matches = np.array([fld == field for fld in msmd.fieldnames()], dtype=bool)
+    field_ids, = np.where(field_matches)
+
+    antsize = msmd.antennadiameter(0)['value'] # m
+    # because we're working with line-split data, we assume the reffreq comes
+    # from spw 0
+    freq = msmd.reffreq(spw)['m0']['value'] # Hz
+    # go a little past the first null in each direction
+    primary_beam = 1.25 * freq/299792458.0 / antsize
+    pb_pix = primary_beam / pixscale
+
+    ptgctrs = [msmd.phasecenter(ii) for ii in field_ids]
+    furthest_ra_pix_plus = np.max([pc['m0']['value']*180/np.pi+pb_pix-cen_ra for pc in ptgctrs])
+    furthest_ra_pix_minus = np.min([pc['m0']['value']*180/np.pi-pb_pix-cen_ra for pc in ptgctrs])
+    furthest_dec_pix_plus = np.max([pc['m1']['value']*180/np.pi+pb_pix-cen_dec for pc in ptgctrs])
+    furthest_dec_pix_minus = np.min([pc['m1']['value']*180/np.pi-pb_pix-cen_dec for pc in ptgctrs])
+
+    msmd.close()
+
+    dra,ddec = furthest_ra_pix_plus-furthest_ra_pix_minus, furthest_dec_pix_plus-furthest_dec_pix_minus
+
+    # go to the next multiple of 20, since it will come up with _something_ when you do 6/5 or 5/4 * n
+    return dra-(dra%20)+20, ddec-(ddec%20)+20
+
+
+def determine_imsize(ms, field, phasecenter, spw=0, pixscale=0.05):
+
+    if isinstance(ms, list):
+        results = [determine_imsize(vis, field, phasecenter, spw, pixscale) for vis in ms]
+
+        dra = np.max([ra for ra, dec in results])
+        ddec = np.max([dec for ra, dec in results])
+    else:
+        dra,ddec = [determine_imsize(vis, field, phasecenter, spw, pixscale) for vis in ms]
+
+    return dra, ddec
+
 # set the 'chanchunks' parameter globally.
 # CASAguides recommend chanchunks=-1, but this resulted in:
 # 2018-09-05 23:16:34     SEVERE  tclean::task_tclean::   Exception from task_tclean : Invalid Gridding/FTM Parameter set : Must have at least 1 chanchunk
@@ -76,8 +152,13 @@ for band in to_image:
                                                                            suffix))
 
 
-            imsize = [3000, 3000]
-            cellsize = ['0.05arcsec', '0.05arcsec']
+            coosys,racen,deccen = determine_phasecenter(ms=vis, field=field)
+            phasecenter = "{0} {1} deg {2} deg".format(coosys, racen, deccen)
+            pixscale = 0.05
+            imsize = list(determine_imsize(ms=vis, field=field,
+                                           phasecenter=(racen,deccen), spw=0,
+                                           pixscale=pixscale)
+            cellsize = ['{0}arcsec'.format(pixscale)] * 2
 
             dirty_tclean_made_residual = False
 
@@ -93,6 +174,7 @@ for band in to_image:
                        outframe='LSRK',
                        veltype='radio',
                        niter=0,
+                       phasecenter=phasecenter,
                        # don't use these for dirty:
                        #usemask='auto-multithresh',
                        #scales=[0,3,9,27,81],
@@ -133,6 +215,7 @@ for band in to_image:
                        veltype='radio',
                        niter=2000,
                        threshold=threshold,
+                       phasecenter=phasecenter,
                        usemask='auto-multithresh',
                        # the sidelobethreshold is very awkward/wrong with 7m+12m
                        # combined data.  Instead, favor the more direct
