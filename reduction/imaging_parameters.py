50504e2967a8515e34111bf4db701dbb1b97aaf4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-03-11 17:57:54 -0600

    add an imaging parameters file

---
+++ reduction/imaging_parameters.py
@@ -0,0 +1,14 @@
+imaging_parameter = {
+    'W51-E_B6_12M_robust0': {'threshold': '0.2mJy',
+                             'pblimit': 0.1,
+                             'niter': 10000,
+                             'robust': 0,
+                             'weighting': 'briggs',
+                             'scales': [0,3,9,27,81],
+                             'gridder': 'mosaic',
+                             'specmode': 'mfs',
+                             'deconvolver': 'mtmfs',
+                             'nterms': 2,
+                             'niter': 10000,
+                            }
+}
