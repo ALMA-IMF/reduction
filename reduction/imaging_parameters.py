439b85d38fd4fab4a511e0ec32231c211e0afdf7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-06-26 18:34:24 -0600

    make w51e-b3 a little less aggressive

--- reduction/imaging_parameters.py
+++ reduction/imaging_parameters.py
@@ -38,7 +38,7 @@ imaging_parameters_nondefault = {
     'W51-E_B6_12M_robust0': {'threshold': '1mJy', # RMS ~0.5-0.6 mJy
                              'scales': [0,3,9,27],
                             },
-    'W51-E_B3_12M_robust0': {'threshold': '0.1mJy', # RMS ~0.1-0.4 mJy
+    'W51-E_B3_12M_robust0': {'threshold': '0.15mJy', # RMS ~0.1-0.4 mJy
                              'scales': [0,3,9,27],
                             },
     'W51-E_B3_12M_robust2': {'threshold': '3mJy',
