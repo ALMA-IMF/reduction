3bf57c3eba300d7d80c349bfb240e02de9145116
Hongli Liu <hongliliu2012@gmail.com>
2019-08-23 13:12:42 -0300

    line parameters fit  more fields

--- reduction/imaging_parameters.py
+++ reduction/imaging_parameters.py
@@ -157,12 +157,16 @@ line_imaging_parameters = {"{0}_{1}_{2}_robust{3}{4}".format(field, band, array,
                           }
 
 
-line_parameters = {'n2hp'：
-                {'restfreq':'93.173700GHz',
-                'vlsr':'-18km/s', # the systematic lsr velocity
-                'cubewidth':'32km/s', # the velocity width you want for the final line cube
-                },
-            }
+line_parameters = {'G353.41': {'n2hp'：{'restfreq':'93.173700GHz',
+                                        'vlsr':'-18km/s', # the systematic lsr velocity
+                                        'cubewidth':'32km/s', # the velocity width you want for the final line cube
+                                        },
+                               'temp'：{'restfreq':'93.173700GHz',
+                                        'vlsr':'-18km/s', # the systematic lsr velocity
+                                        'cubewidth':'32km/s', # the velocity width you want for the final line cube
+                                        },
+                              }
+                  }
 
 # use the continuum image as the startmodel for the non-contsub'd data
 line_imaging_parameters['W51-E_B6_12M_robust0']['startmodel'] = 'imaging_results/W51-E_B6_uid___A001_X1296_X215_continuum_merged_12M_robust0_selfcal7.model.tt0'
