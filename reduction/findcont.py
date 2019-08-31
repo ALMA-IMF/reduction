751267c61d30e2489a40dd2ad1c76bc58b0ad690
Adam Ginsburg <keflavich@gmail.com>
2019-08-30 20:45:37 -0600

    Update findcont.py

--- reduction/findcont.py
+++ reduction/findcont.py
@@ -4,7 +4,7 @@ from spectral_cube import SpectralCube, OneDSpectrum
 from scipy.ndimage import find_objects
 import pylab as pl
 
-def find_and_plot_cont(basename):
+def find_and_plot_cont(basename, background='mean'):
 
     spec = OneDSpectrum.from_hdu(fits.open(basename+'.maxspec.fits'))
     clipped = stats.sigma_clip(spec.value, sigma=1.8, stdfunc=stats.mad_std)
@@ -18,6 +18,7 @@ def find_and_plot_cont(basename):
     clipped = stats.sigma_clip(spec.value, sigma=1.8, stdfunc=stats.mad_std)
     meansel = clipped.mask
 
+    spec = OneDSpectrum.from_hdu(fits.open(basename+'.{0}spec.fits'.format(background)))
     
     spec.quicklook()
     pl.plot(spec.spectral_axis, clipped, linewidth=4, alpha=0.75, zorder=10, color='r')
