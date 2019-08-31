e08a41a3b0c1848a81508078cea9e0a286522194
Adam Ginsburg <keflavich@gmail.com>
2019-08-30 20:41:00 -0600

    New findcont script
    New tool to identify the continuum from the mean spectrum from a spectral cube.  The mean is probably (?) best, but it may well depend on the pointing...

---
+++ reduction/findcont.py
@@ -0,0 +1,29 @@
+from astropy.io import fits
+from astropy import stats
+from spectral_cube import SpectralCube, OneDSpectrum
+from scipy.ndimage import find_objects
+import pylab as pl
+
+def find_and_plot_cont(basename):
+
+    spec = OneDSpectrum.from_hdu(fits.open(basename+'.maxspec.fits'))
+    clipped = stats.sigma_clip(spec.value, sigma=1.8, stdfunc=stats.mad_std)
+    maxsel = clipped.mask
+
+    spec = OneDSpectrum.from_hdu(fits.open(basename+'.medianspec.fits'))
+    clipped = stats.sigma_clip(spec.value, sigma=1.8, stdfunc=stats.mad_std)
+    medsel = clipped.mask
+
+    spec = OneDSpectrum.from_hdu(fits.open(basename+'.meanspec.fits'))
+    clipped = stats.sigma_clip(spec.value, sigma=1.8, stdfunc=stats.mad_std)
+    meansel = clipped.mask
+
+    
+    spec.quicklook()
+    pl.plot(spec.spectral_axis, clipped, linewidth=4, alpha=0.75, zorder=10, color='r')
+    clipped.mask = medsel
+    pl.plot(spec.spectral_axis, clipped, linewidth=3.5, alpha=0.75, zorder=-1)
+    clipped.mask = maxsel
+    pl.plot(spec.spectral_axis, clipped, linewidth=3, alpha=0.75, zorder=11)
+
+    return find_objects(meansel)
