4f7049c5eace60cd9684e363f744468f66aa7dd7
Fernando Olguin <f.olguin.ch@gmail.com>
2019-10-15 21:42:30 +0800

    Save model and residual FITS

--- reduction/continuum_imaging.py
+++ reduction/continuum_imaging.py
@@ -322,6 +322,8 @@ for continuum_ms in continuum_mses:
 
             exportfits(imname+".image.tt0", imname+".image.tt0.fits")
             exportfits(imname+".image.tt0.pbcor", imname+".image.tt0.pbcor.fits")
+            exportfits(imname+".model.tt0", imname+".model.tt0.fits")
+            exportfits(imname+".residual.tt0", imname+".residual.tt0.fits")
         else:
             logprint("Skipping completed file {0}".format(imname), origin='almaimf_cont_imaging')
 
@@ -398,6 +400,8 @@ for continuum_ms in continuum_mses:
 
             exportfits(imname+".image.tt0", imname+".image.tt0.fits")
             exportfits(imname+".image.tt0.pbcor", imname+".image.tt0.pbcor.fits")
+            exportfits(imname+".model.tt0", imname+".model.tt0.fits")
+            exportfits(imname+".residual.tt0", imname+".residual.tt0.fits")
         else:
             logprint("Skipping completed file {0}".format(imname),
                      origin='almaimf_cont_imaging')
