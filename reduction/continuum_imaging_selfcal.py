e916955d6bebad96a67f7f8c890e48a334e66ebe
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-02-08 08:23:06 -0500

    account for 7m-only dish separation appropriately

--- reduction/continuum_imaging_selfcal.py
+++ reduction/continuum_imaging_selfcal.py
@@ -310,7 +310,7 @@ for continuum_ms in continuum_mses:
                                                 exclude_7m=exclude_7m,
                                                 only_7m=only_7m,
                                                 spw='all',
-                                                pixfraction_of_fwhm=1/4.))
+                                                pixfraction_of_fwhm=1/8. if only_7m else 1/4.))
     imsize = [dra, ddec]
     cellsize = ['{0:0.2f}arcsec'.format(pixscale)] * 2
 
