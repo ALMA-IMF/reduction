9a37d39bac1107c25e52b8755e409e512e2aa0f3
Timea CSENGERI <tcsengeri@iris-c.oasu.local>
2020-04-19 12:56:48 +0200

    Change with G351 and G328 (Partly finished)

--- reduction/imaging_parameters.py
+++ reduction/imaging_parameters.py
@@ -56,6 +56,92 @@ for key in imaging_parameters:
 
 
 imaging_parameters_nondefault = {
+# G351.77
+  'G351.77_B6_7M12M_robust0': {'threshold': {0: '4.0mJy', 1: '0.75mJy', 2: '0.50mJy', 3: '0.25mJy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000},
+                               'maskname': {0: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            1: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            2: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            3: 'G351.77_B6_7M12M_robust-2.crtf'}
+                              },
+
+   'G351.77_B6_7M12M_robust2': {'threshold': {0: '4.0mJy', 1: '0.75mJy', 2: '0.50mJy', 3: '0.25mJy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000},
+                               'maskname': {0: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            1: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            2: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            3: 'G351.77_B6_7M12M_robust-2.crtf'}
+                              },
+
+  'G351.77_B6_7M12M_robust-2': {'threshold': {0: '4.0mJy', 1: '0.75mJy', 2: '0.50mJy', 3: '0.25mJy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000},
+                               'maskname': {0: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            1: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            2: 'G351.77_B6_7M12M_robust-2.crtf',
+                                            3: 'G351.77_B6_7M12M_robust-2.crtf'}
+                              },
+
+                                                #rms:6e-4
+   'G351.77_B6_12M_robust0': {'threshold': {0: '12e-4Jy', 1: '12e-4Jy', 2: '12e-4Jy', 3: '12e-4Jy',4: '12e-4Jy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000, 4: 18000},
+                               'maskname': {0: 'G351.77_B6_12M_robust2.crtf',
+                                            1: 'G351.77_B6_12M_robust2.crtf',
+                                            2: 'G351.77_B6_12M_robust2.crtf',
+                                            3: 'G351.77_B6_12M_robust2.crtf',
+                                            4: 'G351.77_B6_12M_robust2.crtf'}
+                              },
+ 
+                                               #rms: 5e-4
+   'G351.77_B6_12M_robust2': {'threshold': {0: '10e-4Jy', 1: '10e-4Jy', 2: '10e-4Jy', 3: '10e-4Jy', 4: '10e-4Jy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000, 4:18000},
+                               'maskname': {0: 'G351.77_B6_12M_robust2.crtf',
+                                            1: 'G351.77_B6_12M_robust2.crtf',
+                                            2: 'G351.77_B6_12M_robust2.crtf',
+                                            3: 'G351.77_B6_12M_robust2.crtf',
+                                            4: 'G351.77_B6_12M_robust2.crtf'}
+                              },
+                                                 # rms: 7.2e-4
+   'G351.77_B6_12M_robust-2': {'threshold': {0: '14.4e-4Jy', 1: '14.4e-4Jy', 2: '14.4e-4Jy', 3: '14.4e-4Jy', 4: '14.4e-4Jy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000, 4:18000},
+                               'maskname': {0: 'G351.77_B6_12M_robust-2.crtf',
+                                            1: 'G351.77_B6_12M_robust-2.crtf',
+                                            2: 'G351.77_B6_12M_robust-2.crtf',
+                                            3: 'G351.77_B6_12M_robust-2.crtf',
+                                            4: 'G351.77_B6_12M_robust-2.crtf'}
+                              },
+
+
+
+
+ 'G351.77_B3_12M_robust-2': {'threshold': {0: '8e-4Jy', 1: '8e-4Jy', 2: '8e-4Jy', 3: '8e-4Jy', 4: '8e-4Jy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000, 4: 18000},
+                               'maskname': {0: 'G351.77_B3_12M_robust2.crtf',
+                                            1: 'G351.77_B3_12M_robust2.crtf',
+                                            2: 'G351.77_B3_12M_robust2.crtf',
+                                            3: 'G351.77_B3_12M_robust2.crtf',
+                                            4: 'G351.77_B3_12M_robust2.crtf'}
+                              },
+                                                # rms: 2.7e-4
+   'G351.77_B3_12M_robust2': {'threshold': {0: '5.4e-4Jy', 1: '5.4e-4Jy', 2: '5.4e-4Jy', 3: '5.4e-4Jy', 4: '5.4e-4Jy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000, 4:18000},
+                               'maskname': {0: 'G351.77_B3_12M_robust2.crtf',
+                                            1: 'G351.77_B3_12M_robust2.crtf',
+                                            2: 'G351.77_B3_12M_robust2.crtf',
+                                            3: 'G351.77_B3_12M_robust2.crtf',
+                                            4: 'G351.77_B3_12M_robust2.crtf'}
+                              },
+  'G351.77_B3_12M_robust0': {'threshold': {0: '7e-4Jy', 1: '7e-4Jy', 2: '7e-4Jy', 3: '7e-4Jy', 4: '7e-4Jy'},
+                               'niter': {0: 1000, 1: 3000, 2: 9000, 3: 18000, 4:18000},
+                               'maskname': {0: 'G351.77_B3_12M_robust2.crtf',
+                                            1: 'G351.77_B3_12M_robust2.crtf',
+                                            2: 'G351.77_B3_12M_robust2.crtf',
+                                            3: 'G351.77_B3_12M_robust2.crtf',
+                                            4: 'G351.77_B3_12M_robust2.crtf'}
+                              },
+
+
+
+
         ##### G333.60 #####
     'G333.60_B3_12M_robust0': {'threshold': {0: '0.8mJy', 1: '0.8mJy', 2: '0.4mJy', 3: '0.2mJy', 4: '0.1mJy'},
                                'niter': {0: 2000, 1: 2000, 2: 6000, 3: 18000, 4: 36000},
