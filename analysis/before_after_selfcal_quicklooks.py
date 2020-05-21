064dc759cb1faa5988d9e501fabdab6d77940a5a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-05-21 13:48:52 -0400

    fix import statement

--- analysis/before_after_selfcal_quicklooks.py
+++ analysis/before_after_selfcal_quicklooks.py
@@ -13,7 +13,7 @@ from astropy import wcs
 import pylab as pl
 
 
-from compare_images import make_comparison_images
+from compare_images import make_comparison_image
 
 def get_selfcal_number(fn):
     numberstring = fn.split("selfcal")[1][0]
