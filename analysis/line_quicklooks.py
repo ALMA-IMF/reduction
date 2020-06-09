664f1a8a4255f6e96c59148c43990652dd563442
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-06-09 08:10:15 -0400

    analysis tools, and new delivery status grid

--- analysis/line_quicklooks.py
+++ analysis/line_quicklooks.py
@@ -39,7 +39,7 @@ def dt():
     print(f"Elapsed: {now-then}")
     then = now
 
-for field in "G012.80 W43-MM2 G333.60 G327.29 G338.93 W51-E G353.41 G008.67 G337.92 W43-MM3 G328.25 G351.77 W43-MM1 G010.62 W51-IRS2".split():
+for field in "W43-MM2 G333.60 G327.29 G338.93 W51-E G353.41 G008.67 G337.92 W43-MM3 G328.25 G351.77 W43-MM1 G010.62 W51-IRS2 G012.80".split():
     for band in (3,6):
         for config in ('7M12M', '12M'):
             for line in default_lines:
