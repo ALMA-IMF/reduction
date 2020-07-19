34837342313854b8d600fb16636f1d782f41f9a8
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-07-19 13:00:48 -0400

    add another slurm script

---
+++ reduction/slurm_scripts/run_continuum_imaging_selfcal_slurm_submitter.sh
@@ -0,0 +1,67 @@
+export FIELD_ID=$1
+
+
+
+export ONLY_7M=False
+export EXCLUDE_7M=True
+export BAND_TO_IMAGE=B6
+
+#CMD=/orange/adamginsburg/ALMA_IMF/reduction/reduction/run_continuum_imaging_slurm.sh
+#export LOGFILENAME="casa_log_continuum_${FIELD_ID}_${BAND_TO_IMAGE}_12M_$(date +%Y-%m-%d_%H_%M_%S).log"
+#jobid=$(sbatch --output=${FIELD_ID}_${BAND_TO_IMAGE}_12Mcont_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_12Mcont --export=ALL $CMD)
+#echo ${jobid##* }
+
+CMD=/orange/adamginsburg/ALMA_IMF/reduction/reduction/run_continuum_imaging_selfcal_slurm.sh
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_12M_$(date +%Y-%m-%d_%H_%M_%S).log"
+jobid=$(sbatch --output=${FIELD_ID}_${BAND_TO_IMAGE}_12M_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_12M_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_12M_bsens_$(date +%Y-%m-%d_%H_%M_%S).log"
+export DO_BSENS=True
+jobid=$(sbatch --dependency=afterok:${jobid##* } --output=${FIELD_ID}_${BAND_TO_IMAGE}_12M_bsens_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_12M_bsens_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+export DO_BSENS=False
+
+
+export EXCLUDE_7M=False
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_7M12M_$(date +%Y-%m-%d_%H_%M_%S).log"
+jobid=$(sbatch --dependency=afterok:${jobid##* } --output=${FIELD_ID}_${BAND_TO_IMAGE}_7M12M_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_7M12M_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+
+export EXCLUDE_7M=False
+export ONLY_7M=True
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_7M_$(date +%Y-%m-%d_%H_%M_%S).log"
+jobid=$(sbatch --dependency=afterok:${jobid##* } --output=${FIELD_ID}_${BAND_TO_IMAGE}_7M_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_7M_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+
+
+
+export ONLY_7M=False
+export EXCLUDE_7M=True
+export BAND_TO_IMAGE=B3
+#CMD=/orange/adamginsburg/ALMA_IMF/reduction/reduction/run_continuum_imaging_slurm.sh
+#export LOGFILENAME="casa_log_continuum_${FIELD_ID}_${BAND_TO_IMAGE}_12M_$(date +%Y-%m-%d_%H_%M_%S).log"
+#jobid=$(sbatch --output=${FIELD_ID}_${BAND_TO_IMAGE}_12Mcont_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_12Mcont --export=ALL $CMD)
+#echo ${jobid##* }
+
+CMD=/orange/adamginsburg/ALMA_IMF/reduction/reduction/run_continuum_imaging_selfcal_slurm.sh
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_12M_$(date +%Y-%m-%d_%H_%M_%S).log"
+jobid=$(sbatch --output=${FIELD_ID}_${BAND_TO_IMAGE}_12M_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_12M_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_12M_bsens_$(date +%Y-%m-%d_%H_%M_%S).log"
+export DO_BSENS=True
+jobid=$(sbatch --dependency=afterok:${jobid##* } --output=${FIELD_ID}_${BAND_TO_IMAGE}_12M_bsens_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_12M_bsens_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+export DO_BSENS=False
+
+export EXCLUDE_7M=False
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_7M12M_$(date +%Y-%m-%d_%H_%M_%S).log"
+jobid=$(sbatch --dependency=afterok:${jobid##* } --output=${FIELD_ID}_${BAND_TO_IMAGE}_7M12M_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_7M12M_selfcal --export=ALL $CMD)
+echo ${jobid##* }
+
+export EXCLUDE_7M=False
+export ONLY_7M=True
+export LOGFILENAME="casa_log_selfcalcont_${FIELD_ID}_${BAND_TO_IMAGE}_7M_$(date +%Y-%m-%d_%H_%M_%S).log"
+jobid=$(sbatch --dependency=afterok:${jobid##* } --output=${FIELD_ID}_${BAND_TO_IMAGE}_7M_selfcal_%j.log --job-name=${FIELD_ID}_${BAND_TO_IMAGE}_7M_selfcal --export=ALL $CMD)
+echo ${jobid##* }
