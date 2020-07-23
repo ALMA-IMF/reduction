defead4fef943c7791ba4676ef81f096b9c944b6
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-07-23 08:02:33 -0400

    added linker for July 2020 internal release

---
+++ analysis/link_files_July2020release.py
@@ -0,0 +1,66 @@
+import glob
+from os import symlink, chdir, mkdir
+from pathlib import Path
+import os
+
+spws = {3: list(range(4)),
+        6: list(range(7)),}
+
+default_lines = {'n2hp': '93.173700GHz',
+                 'sio': '217.104984GHz',
+                 'h2co303': '218.222195GHz',
+                 '12co': '230.538GHz',
+                 'h30a': '231.900928GHz',
+                 'h41a': '92.034434GHz',
+                 "c18o": "219.560358GHz",
+                 "ch3cn": "92.26144GHz",
+                 "ch3cch": "102.547983GHz",
+                }
+
+
+dirnames = {'fullcubes_12m': 'spw[0-9]_12M_spw[0-9]',
+            'linecubes_12m': 'spw[0-9]_12M_[!s]',
+            'fullcubes_7m12m': 'spw[0-9]_7M12M_spw[0-9]',
+            'linecubes_7m12m': 'spw[0-9]_7M12M_[!s]',
+            'bsens': 'bsens_12M_*.tt0',
+            'cleanest': 'merged_12M*.tt0',
+            '7m12m': 'merged_7M12M*.tt0',
+            '7m12m_bsens': 'bsens_7M12M*.tt0',
+            '7m': 'merged_7M_*.tt0',
+            '7m_bsens': 'bsens_7M_*.tt0',
+           }
+
+
+basepath = Path('/orange/adamginsburg/ALMA_IMF/2017.1.01355.L/imaging_results/')
+
+releasepath = Path('/orange/adamginsburg/ALMA_IMF/2017.1.01355.L/July2020Release/')
+
+with open(basepath / '../scigoals/file_list.txt', 'w') as fh1:
+    with open(basepath / '../scigoals/file_tree.txt', 'w') as fh2:
+
+        for field in "G008.67 G337.92 W43-MM3 G328.25 G351.77 G012.80 G327.29 W43-MM1 G010.62 W51-IRS2 W43-MM2 G333.60 G338.93 W51-E G353.41".split():
+            if not os.path.exists(basepath / field):
+                mkdir(basepath / field)
+            for band in (3,6):
+                bandpath = Path(f"B{band}")
+                if not os.path.exists(basepath / field / bandpath):
+                    mkdir(basepath / field / bandpath)
+                for dirname, globstr in dirnames.items():
+                    if not os.path.exists(basepath / field / bandpath / dirname):
+                        mkdir(basepath / field / bandpath / dirname)
+                    cwd = os.getcwd()
+                    chdir(basepath / field / bandpath / dirname)
+                    globbo = str(basepath / f"{field}_B{band}*{globstr}*")
+                    filelist = glob.glob(globbo)
+                    #print(field, band, dirname, config, filelist)
+                    for fn in filelist:
+                        #print(f"Linking {dotdot / fn} to {os.getcwd()}")
+                        basename = os.path.basename(fn)
+                        if not os.path.exists(basename):
+                            symlink(fn, basename)
+                        elif not os.path.exists(os.readlink(basename)):
+                            os.unlink(basename)
+                            symlink(fn, basename)
+                        fh1.write(os.path.realpath(basename) + "\n")
+                        fh2.write(os.path.join(os.getcwd(), basename) + "\n")
+                    chdir(cwd)
