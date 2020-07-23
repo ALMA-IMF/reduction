ab374ff339eeaaab2d0eec9895ef7e1f590ac06e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-07-23 08:38:07 -0400

    add another release stats script

--- analysis/do_all_stats_stuff.py
+++ analysis/do_all_stats_stuff.py
@@ -7,6 +7,8 @@ import runpy
 import pylab as pl
 runpy.run_path('delivery_status.py', run_name="__main__")
 runpy.run_path('imstats.py', run_name="__main__")
+runpy.run_path('before_after_selfcal_quicklooks_July2020_release.py', run_name="__main__")
+pl.close('all')
 runpy.run_path('before_after_selfcal_quicklooks_Feb2020_release.py', run_name="__main__")
 pl.close('all')
 runpy.run_path('dirty_selfcal_compare.py', run_name="__main__")
