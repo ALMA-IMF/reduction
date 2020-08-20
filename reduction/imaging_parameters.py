0df59a7ca2ae3d4d62a61bcc0f41b1b2700f06c7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-08-20 10:09:40 -0400

    set fixed threshold for w51-e too

--- reduction/imaging_parameters.py
+++ reduction/imaging_parameters.py
@@ -2755,12 +2755,12 @@ line_imaging_parameters_custom = {
     "W51-E_B6_12M_robust0": {
         "usemask": "auto-multithresh",
         "sidelobethreshold": 1.0,
-        "threshold": "16mJy", # sigma is ~4 mJy
+        "threshold": "16mJy",  # sigma is ~4 mJy
         "startmodel": "W51-E_B6_uid___A001_X1296_X213_continuum_merged_12M_robust0_selfcal6_finaliter",
     },
     "W51-E_B3_12M_robust0": {
         "startmodel": "W51-E_B3_uid___A001_X1296_X10b_continuum_merged_7M_robust0_selfcal4_finaliter",
-        "threshold": "5sigma",
+        "threshold": "4mJy",  # sigma is ~0.8 mJy
     },
 }
 
