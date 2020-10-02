8e2a70cfe7048d9f5709ed7655b736c8e840216b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-01 21:04:18 -0400

    add more different robust parameters so we can measure them

--- reduction/imaging_parameters.py
+++ reduction/imaging_parameters.py
@@ -2862,8 +2862,6 @@ line_imaging_parameters_custom = {
         "startmodel": "W43-MM3_B3_uid___A001_X1296_X12f_continuum_merged_12M_robust0_selfcal4_finaliter",
     },
     "W51-E_B6_12M_robust0": {
-        "usemask": "auto-multithresh",
-        "sidelobethreshold": 1.0,
         "pblimit": 0.1,
         "threshold": "16mJy",  # sigma is ~4 mJy
         "startmodel": "W51-E_B6_uid___A001_X1296_X213_continuum_merged_12M_robust0_selfcal6_finaliter",
