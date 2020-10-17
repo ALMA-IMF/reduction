f23b0d50bc008688be26b3c6194d9a2bdd2c5394
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-17 17:20:15 -0400

    more order changing

--- analysis/line_quicklooks.py
+++ analysis/line_quicklooks.py
@@ -62,7 +62,7 @@ def dt():
     print(f"Elapsed: {now-then}")
     then = now
 
-for field in "W43-MM2 G327.29 G338.93 W51-E G353.41 G008.67 G337.92 W43-MM3 G328.25 G351.77 W43-MM1 G010.62 W51-IRS2 G012.80 G333.60".split():
+for field in "G328.25 G351.77 W51-IRS2 W43-MM2 G327.29 G338.93 W51-E G353.41 G008.67 G337.92 W43-MM3 W43-MM1 G010.62 G012.80 G333.60".split():
     for band in (3,6):
         for config in ('7M12M', '12M'):
             for line in default_lines:
