5b27facf99e3fdc1aca7dd054352acf9bb8ca263
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-17 18:30:29 -0400

    name scheme

--- analysis/cube_spectral_quicklooks.py
+++ analysis/cube_spectral_quicklooks.py
@@ -118,14 +118,16 @@ for field in "G353.41 G008.67 G337.92 W51-E W43-MM3 G328.25 G351.77 W43-MM1 G010
                     #cube = cube.rechunk(save_to_tmp_dir=True)
                     print(cube)
 
+                    underscore = "_" if linename or suffix else ""
+
                     print('computing max(axis=(1,2))')
-                    mxspecfn = spectra_dir / f"{field}_B{band}_spw{spw}_{config}_{linename}{suffix}_max.fits"
+                    mxspecfn = spectra_dir / f"{field}_B{band}_spw{spw}_{config}{underscore}{linename}{suffix}_max.fits"
                     if not os.path.exists(mxspecfn):
                         maxspec = cube.max(axis=(1,2))
                         maxspec.write(mxspecfn)
 
                     print('computing mean(axis=(1,2))')
-                    mnspecfn = spectra_dir / f"{field}_B{band}_spw{spw}_{config}_{linename}{suffix}_mean.fits"
+                    mnspecfn = spectra_dir / f"{field}_B{band}_spw{spw}_{config}{underscore}{linename}{suffix}_mean.fits"
                     if not os.path.exists(mnspecfn):
                         meanspec = cube.mean(axis=(1,2))
                         meanspec.write(mnspecfn)
--- analysis/line_quicklooks.py
+++ analysis/line_quicklooks.py
@@ -66,7 +66,7 @@ def dt():
 
 for field in "G328.25 G351.77 W51-IRS2 W43-MM2 G327.29 G338.93 W51-E G353.41 G008.67 G337.92 W43-MM3 W43-MM1 G010.62 G012.80 G333.60".split():
     for band in (3,6):
-        for config in ('7M12M', '12M'):
+        for config in ('12M',): #'7M12M',
             for line in default_lines:
                 for suffix in (".image", ".contsub.image"):
                     globblob = f"{field}_B{band}*_{config}_*{line}{suffix}"
