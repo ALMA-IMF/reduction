ef3b40bdaeab098a924f4801803634a9bb24eeed
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-17 19:23:29 -0400

    add 'mergecontdotdat'script

---
+++ reduction/merge_contdotdat.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+def parse_contdotdat(filepath):
+
+    selections = []
+
+    with open(filepath, 'r') as fh:
+        for line in fh:
+            if "LSRK" in line:
+                selections.append(line.split()[0])
+
+
+    return ";".join(selections)
+
+
+data = {}
+
+for dirpath, dirnames, filenames in os.walk('.'):
+    for fn in filenames:
+        if fn == 'cont.dat':
+            contfn = os.path.join(dirpath, fn)
+            with open(contfn, 'r') as readfh:
+                field = readfh.readline().split()[1]
+            if field in data:
+                data[field] += ";" + parse_contdotdat(contfn)
+            else:
+                data[field] = parse_contdotdat(contfn)
+
+with open('merged_cont.dat.json', 'w') as fh:
+    json.dump(data, fh)
