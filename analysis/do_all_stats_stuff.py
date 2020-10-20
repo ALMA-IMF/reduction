37fc3a116b365511b2b59291c7565bff619baf51
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-20 10:08:32 -0400

    expand the definition of "all stats"

--- analysis/do_all_stats_stuff.py
+++ analysis/do_all_stats_stuff.py
@@ -1,7 +1,5 @@
 """
 Run all of the summary statistics
-
-October 31 (2019) stuff can be commented out later
 """
 import runpy
 import pylab as pl
@@ -17,13 +15,22 @@ runpy.run_path('dirty_selfcal_compare.py', run_name="__main__")
 pl.close('all')
 runpy.run_path('bsens_comparison.py', run_name="__main__")
 pl.close('all')
+runpy.run_path('robust_comparisons.py', run_name="__main__")
+pl.close('all')
 runpy.run_path('selfcal_field_data.py', run_name="__main__")
 pl.close('all')
 # run locall runpy.run_path('latex_table.py', run_name="__main__")
 #runpy.run_path('compare_to_auto.py', run_name="__main__")
 #pl.close('all')
+runpy.run_path('continuum_selections.py', run_name="__main__")
 runpy.run_path('cube_metadata_grid.py', run_name="__main__")
 runpy.run_path('fullcube_quicklooks.py', run_name="__main__")
 runpy.run_path('before_after_selfcal_quicklooks_October31_2019_release.py', run_name="__main__")
 runpy.run_path('7m12m_comparison.py', run_name="__main__")
+pl.close('all')
+runpy.run_path('diagnostic_images.py', run_name="__main__")
+pl.close('all')
+runpy.run_path('diagnostic_spectra.py', run_name="__main__")
+pl.close('all')
+runpy.run_path('plot_uvspectra.py', run_name="__main__")
 pl.close('all')
