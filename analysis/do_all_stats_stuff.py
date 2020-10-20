6f06e840b37d931a94e02322ac8db1a898e91c5f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-20 10:11:05 -0400

    restructure do-all-stats

--- analysis/do_all_stats_stuff.py
+++ analysis/do_all_stats_stuff.py
@@ -3,34 +3,34 @@ Run all of the summary statistics
 """
 import runpy
 import pylab as pl
-runpy.run_path('delivery_status.py', run_name="__main__")
-runpy.run_path('imstats.py', run_name="__main__")
-runpy.run_path('before_after_selfcal_quicklooks_October2020_release.py', run_name="__main__")
-pl.close('all')
+
+scripts = ['delivery_status.py',
+           'imstats.py',
+           'before_after_selfcal_quicklooks_October2020_release.py',
+           'dirty_selfcal_compare.py',
+           'bsens_comparison.py',
+           'robust_comparisons.py',
+           'selfcal_field_data.py',
+           'continuum_selections.py',
+           'cube_metadata_grid.py',
+           'fullcube_quicklooks.py',
+           '7m12m_comparison.py',
+           'diagnostic_images.py',
+           'diagnostic_spectra.py',
+           'plot_uvspectra.py',
+          ]
+
+for scriptname in scripts:
+    print(scriptname)
+    try:
+        runpy.run_path(scriptname, run_name="__main__")
+    except Exception as ex:
+        print(ex)
+    pl.close('all')
+
+
+# run locally runpy.run_path('latex_table.py', run_name="__main__")
 #runpy.run_path('before_after_selfcal_quicklooks_July2020_release.py', run_name="__main__")
-#pl.close('all')
 #runpy.run_path('before_after_selfcal_quicklooks_Feb2020_release.py', run_name="__main__")
-#pl.close('all')
-runpy.run_path('dirty_selfcal_compare.py', run_name="__main__")
-pl.close('all')
-runpy.run_path('bsens_comparison.py', run_name="__main__")
-pl.close('all')
-runpy.run_path('robust_comparisons.py', run_name="__main__")
-pl.close('all')
-runpy.run_path('selfcal_field_data.py', run_name="__main__")
-pl.close('all')
-# run locall runpy.run_path('latex_table.py', run_name="__main__")
+#runpy.run_path('before_after_selfcal_quicklooks_October31_2019_release.py', run_name="__main__")
 #runpy.run_path('compare_to_auto.py', run_name="__main__")
-#pl.close('all')
-runpy.run_path('continuum_selections.py', run_name="__main__")
-runpy.run_path('cube_metadata_grid.py', run_name="__main__")
-runpy.run_path('fullcube_quicklooks.py', run_name="__main__")
-runpy.run_path('before_after_selfcal_quicklooks_October31_2019_release.py', run_name="__main__")
-runpy.run_path('7m12m_comparison.py', run_name="__main__")
-pl.close('all')
-runpy.run_path('diagnostic_images.py', run_name="__main__")
-pl.close('all')
-runpy.run_path('diagnostic_spectra.py', run_name="__main__")
-pl.close('all')
-runpy.run_path('plot_uvspectra.py', run_name="__main__")
-pl.close('all')
