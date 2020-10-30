2c8e5fe19839e1ccc6f9ad4b32c516d022228981
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-10-30 16:42:26 -0400

    small path tweak

--- analysis/make_all_selfcal_anims.py
+++ analysis/make_all_selfcal_anims.py
@@ -5,11 +5,11 @@ import shutil
 
 os.chdir('/orange/adamginsburg/ALMA_IMF/2017.1.01355.L/imaging_results/')
 
-for fn in glob.glob("*_12M_robust0**finaliter*.image.tt0.fits"):
+for fn in glob.glob("*_12M_robust0_*finaliter*.image.tt0.fits"):
     imname = fn.split("_selfcal")[0]
     print(imname)
     make_anim(imname)
     make_anim_single(imname, 'image')
 
-    shutil.copy(f'{imname}_selfcal_anim.gif', '/bio/web/secure/adamginsburg/ALMA-IMF/Feb2020/selfcal_animations/')
+    shutil.copy(f'{imname}_selfcal_anim.gif', '/bio/web/secure/adamginsburg/ALMA-IMF/October2020Release/selfcal_animations/')
     #shutil.copy(f'{imname}_image_selfcal_anim.gif', '/bio/web/secure/adamginsburg/ALMA-IMF/Feb2020/selfcal_animations/')
