8072dd6e9b2f7fec64ef5ab9a8b198cc2fdab9be
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-11-16 11:40:15 -0500

    include masks in release

--- analysis/make_oct2020release.py
+++ analysis/make_oct2020release.py
@@ -18,11 +18,12 @@ for field in "G008.67 G337.92 W43-MM3 G328.25 G351.77 G012.80 G327.29 W43-MM1 G0
             itpath = releasepath / field / band / imtype
             itpath.mkdir(parents=True, exist_ok=True)
 
-            for suffix in ('image.tt0.fits', 'image.tt0.pbcor.fits', "image.tt0", "model.tt0", "image.tt1", "model.tt1", "psf.tt0", "psf.tt1", "residual.tt0", "residual.tt1"):
+            for suffix in ('image.tt0.fits', 'image.tt0.pbcor.fits', "image.tt0", "model.tt0", "image.tt1", "model.tt1", "psf.tt0", "psf.tt1", "residual.tt0", "residual.tt1", "mask"):
 
                 for globstr in (f"{field}*_{band}_*{itgl}*robust0_*selfcal[0-9]*finaliter.{suffix}",
                                 f"{field}*_{band}_*{itgl}*robust0_*preselfcal.{suffix}",
                                 f"{field}*_{band}_*{itgl}*robust0_*preselfcal_finalmodel.{suffix}",
+                                f"{field}_{band}_*_robust0_12M_mask.{suffix}", # use suffix here to avoid re-copying
                                ):
 
                     files = glob.glob(str(basepath / globstr))
