399d0afef40632030d9bb98ecea59b4096425914
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-01-26 16:56:58 -0500

    remove amp-only calibration

--- reduction/imaging_parameters.py
+++ reduction/imaging_parameters.py
@@ -1208,7 +1208,7 @@ selfcal_pars_custom = {
         2: {"calmode": "p", "gaintype": "T", "minsnr": 5, "solint": "1200s", "solnorm": False},
         3: {"calmode": "p", "gaintype": "T", "minsnr": 5, "solint": "300s", "solnorm": False},
         4: {"calmode": "p", "gaintype": "T", "minsnr": 5, "solint": "300s", "solnorm": False},
-        5: {"calmode": "a", "gaintype": "T", "minsnr": 5, "solint": "inf", "solnorm": False},
+        5: {"calmode": "p", "gaintype": "T", "minsnr": 5, "solint": "inf", "solnorm": False},
     },
     "G012.80_B3_12M_robust2": {
         1: {"calmode": "p", "gaintype": "T", "solint": "inf", "solnorm": True},
@@ -1263,7 +1263,7 @@ selfcal_pars_custom = {
         2: {"calmode": "p", "gaintype": "G", "minsnr": 5, "solint": "inf", "solnorm": False},
         3: {"calmode": "p", "gaintype": "G", "minsnr": 5, "solint": "1200s", "solnorm": False},
         4: {"calmode": "p", "gaintype": "G", "minsnr": 4, "solint": "600s", "solnorm": False},
-        5: {"calmode": "a", "gaintype": "G", "minsnr": 5, "solint": "inf", "solnorm": False},
+        5: {"calmode": "p", "gaintype": "G", "minsnr": 5, "solint": "inf", "solnorm": False},
     },
     "G012.80_B6_12M_robust2": {
         1: {"calmode": "p", "gaintype": "T", "solint": "inf", "solnorm": True},
