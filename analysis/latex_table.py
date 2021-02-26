baf9c991a1627efef595776116fe0c9d394ace75
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-02-26 09:09:28 -0500

    add amplitude to tables

--- analysis/latex_table.py
+++ analysis/latex_table.py
@@ -49,7 +49,7 @@ print(len(wtbl))
 print(wtbl)
 
 # strip preceding "sc" from selfcal numbers
-wtbl['selfcaliter'] = Column(data=[int(x[2:]) for x in wtbl['selfcaliter']])
+wtbl['selfcaliter'] = Column(data=[row['selfcaliter'][2:]+("a" if row['has_amp'] else "") for row in wtbl])
 
 # SensVsReq can be populated with either pre- or post-; we want post
 wtbl['SensVsReqPost'] = wtbl['mad_sample_post'] / wtbl['Req_Sens'] * 1000
@@ -125,6 +125,7 @@ latexdict['col_align'] = 'l'*len(fwtbl.columns)
 latexdict['tabletype'] = 'table*'
 latexdict['tablefoot'] = ("}\par\n"
                           "$n_{sc}$ is the number of self-calibration iterations adopted.  "
+                          "Those with a final iteration of amplitude self-calibration are denoted with the 'a' suffix.  "
                           "$\\theta_{maj}, \\theta_{min}$, and BPA give the major and minor full-width-half-maxima (FWHM) of the synthesized beams.  "
                           "$\\theta_{req}$ is the requested beam size, "
                           "and $\\Omega_{syn}^{1/2}/\\Omega_{req}^{1/2}$ gives the ratio of the synthesized to the "
