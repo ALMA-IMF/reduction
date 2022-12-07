12c338a624e438bb3cc1e3949ee367fd15c0d392
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-07 23:47:40 +0000

    Adding tarfile member sanitization to extractall()

--- reduction/retrieve_data_run_pipeline.py
+++ reduction/retrieve_data_run_pipeline.py
@@ -99,7 +99,26 @@ for filename in data:
     print(filename)
     if 'tar.gz' == filename[-6:] or 'tgz' == filename[-3:] or 'tar' == filename[-3:]:
         with tarfile.open(filename) as tf:
-            tf.extractall('.')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, ".")
 
 os.chdir('2017.1.01355.L')
 
--- reduction/retrieve_weblogs.py
+++ reduction/retrieve_weblogs.py
@@ -53,7 +53,26 @@ if not os.path.exists('2017.1.01355.L/weblog_tarballs'):
 for logfile in weblog_tarballs:
     print(logfile)
     with tarfile.open(logfile) as tf:
-        tf.extractall('2017.1.01355.L')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, "2017.1.01355.L")
 
 for dirpath, dirnames, filenames in os.walk('.'):
     for fn in filenames:
